63f665e9f0e51a6b753e442786f3e355ba32e572
Mahmoud Reda <113782853+mahmoudreda12@users.noreply.github.com>
2023-03-19 16:09:23 +0200

    Staff class by Mahmoud Reda and Mohamed Mohab

---
+++ src/main/java/com/mycompany/supermarket/Staff.java
@@ -0,0 +1,16 @@
+package com.mycompany.supermarket;
+
+public class Staff {
+    private int salary;
+    
+    public Staff(int salary){
+        this.salary=salary;
+    }
+    
+    public void setSalary(int salary){
+        this.salary=salary;
+}
+    public int getSalary(){
+        return salary;
+    }
+}
