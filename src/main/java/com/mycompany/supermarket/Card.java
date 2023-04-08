3014cd8d55e86087df3c8c83f52d477a597fcda5
Mahmoud Reda <113782853+mahmoudreda12@users.noreply.github.com>
2023-04-08 16:19:43 +0200

    Card class by Mahmoud Reda

---
+++ src/main/java/com/mycompany/supermarket/Card.java
@@ -0,0 +1,30 @@
+package com.mycompany.supermarket;
+
+public class Card {
+    private int code;
+    private String username;
+
+    public Card() {
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+
+    
+    
+    
+}
