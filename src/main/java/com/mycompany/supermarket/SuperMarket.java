de5847210a863844eb520762b83973f786654bf5
Rinzler <divosilver.144@gmail.com>
2023-04-16 23:52:59 +0200

    added some comments

--- src/main/java/com/mycompany/supermarket/SuperMarket.java
+++ src/main/java/com/mycompany/supermarket/SuperMarket.java
@@ -19,6 +19,18 @@ public class SuperMarket {
                }
            });
            
+           //- the program will not be operating in real time
+           //we will need to save the time in a file
+           //or
+           //- the program runs in the real time, we have the ability to move forward or backwards
+           //in time to be able to see the effects imply on time passing
+           //-------------------
+           //we need to
+           //check 2 dates equallity: product expiration
+           //check the difference between 2 dates: toGolden() condition
+           //check for a spacific month and day : golden client birthday
+           //check the number of the week in year for the weekly report
+           
            
     }
 }
