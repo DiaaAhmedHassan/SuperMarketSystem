0b1e6dac76955d0d8b67e173d78ac26f9361f1b3
Rinzler <divosilver.144@gmail.com>
2023-04-29 23:17:05 +0200

    modifications to the pos

--- src/main/java/com/mycompany/supermarket/PointOfSale.java
+++ src/main/java/com/mycompany/supermarket/PointOfSale.java
@@ -1,17 +1,21 @@
 package com.mycompany.supermarket;
 
+import javax.swing.JOptionPane;
+
 public class PointOfSale {
-    private String ID;
-    private Card cards;
+//    public static pocNum = 5;
+    private String ID;//p1, p2, p3, p4 or p5
+    private Card[] shift1 = new Card[5];
+    private Card[] shift2 = new Card[5];
+    private Card[] shift3 = new Card[5];
 
     public PointOfSale() {
     }
 
-    public PointOfSale(String ID, Card cards) {
+    public PointOfSale(String ID) {
         this.ID = ID;
-        this.cards = cards;
     }
-
+    
     public String getID() {
         return ID;
     }
@@ -19,13 +23,34 @@ public class PointOfSale {
     public void setID(String ID) {
         this.ID = ID;
     }
-
-    public Card getCards() {
-        return cards;
-    }
-
-    public void setCards(Card cards) {
-        this.cards = cards;
-    }
-    
+    public void addMember(Card memberCard, int shift)
+    {
+        Card[] cards = null;
+        switch(shift)
+        {
+            case 1:
+                cards = shift1;
+            break;
+            case 2:
+                cards = shift2;
+            break;
+            case 3:
+                cards = shift3;
+            break;
+        }
+        
+        for(int i = 0; i < cards.length; i++)
+        {
+            if(cards[i] == null)
+            {
+                cards[i] = memberCard;
+                break;
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "can't add this card", "null object", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        
+    } 
 }
